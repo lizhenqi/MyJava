f2a11ddfd4597a301a24e4de3f19239f1e22736c
lizhenqi <565400895@qq.com>
2016-06-08 17:25:49 +0800

    2016-6-8提交

---
+++ javase/src/com/kaishengit/Test.java
@@ -0,0 +1,10 @@
+package com.kaishengit;
+
+/**
+ * Created by Administrator on 2016/6/8.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
