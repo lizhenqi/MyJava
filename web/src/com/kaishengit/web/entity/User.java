993a7dc25e8ea54c7c2340640936bdf7c5007f19
lizhenqi <565400895@qq.com>
2016-06-10 11:19:47 +0800

    2016-6-10第一次提交

--- web/src/com/kaishengit/web/entity/User.java
+++ web/src/com/kaishengit/web/entity/User.java
@@ -1,17 +1,26 @@
 package com.kaishengit.web.entity;
 
 public class User {
-    private int id;
-    private String username;
-    private String address;
-    private String password;
 
-    public int getId() {
-        return id;
+    private int ID;
+    private String username;
+    private String password;
+    private String address;
+
+    public int getID() {
+        return ID;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
@@ -30,22 +39,60 @@ public class User {
         this.address = address;
     }
 
-    public String getUsername() {
-        return username;
+    @Override
+    public String toString() {
+        return "User{" +
+                "ID=" + ID +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
+    //    private int id;
+//    private String username;
+//    private String address;
+//    private String password;
 //
-//    @Override
-//    public String toString() {
-//        return "User[" +
-//                "id=" + id +
-//                ", username='" + username + '\'' +
-//                ", address='" + address + '\'' +
-//                ", password='" + password + '\'' +
-//                ']';
+//    public int getId() {
+//        return id;
 //    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+////
+////    @Override
+////    public String toString() {
+////        return "User[" +
+////                "id=" + id +
+////                ", username='" + username + '\'' +
+////                ", address='" + address + '\'' +
+////                ", password='" + password + '\'' +
+////                ']';
+////    }
 }
--- web/src/com/kaishengit/web/util/ConnectionManager.java
+++ web/src/com/kaishengit/web/util/ConnectionManager.java
@@ -1,14 +1,19 @@
 package com.kaishengit.web.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
+
+
     public static Connection getConnection(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connection= DriverManager.getConnection("jdbc:mysql:///db_21","root","root");
+            Connection connection=DriverManager.getConnection("jdbc:mysql:///db_21","root","root");
             return connection;
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -17,5 +22,43 @@ public class ConnectionManager {
         }
         return null;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static Connection getConnection(){
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection connection= DriverManager.getConnection("jdbc:mysql:///db_21","root","root");
+//            return connection;
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 
 }
